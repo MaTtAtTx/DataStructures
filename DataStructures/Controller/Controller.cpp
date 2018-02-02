e5bcdf83325ad4a17e8efeda21adb2f6443aa2f5
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-02-02 08:49:27 -0700

    Fixed reading from the file, and added a min and max function.

--- DataStructures/Controller/Controller.cpp
+++ DataStructures/Controller/Controller.cpp
@@ -16,11 +16,38 @@ void Controller :: start()
     Timer codeTimer;
     codeTimer.startTimer();
     vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/mjoh7008/Documents/Data Structures/DataStructures/DataStructures/Data/crime.csv");
-    for (int index = 199; index < 216; index++)
+    for (int index = 199; index < 215; index++)
     {
         cout << "Data at spot # " << index + 1 << ": " << myData[index] << endl;
     }
     codeTimer.stopTimer();
     codeTimer.displayInformation();
     
+}
+
+void Controller :: findMaxAndMin()
+{
+    Timer searchTimer;
+    searchTimer.startTimer();
+    vector<CrimeData> myData = FileController :: readCrimeDataToVector("/Users/mjoh7008/Documents/Data Structures/DataStructures/DataStructures/Data/crime.csv");
+    
+    int minIndex = 0;
+    int maxIndex = 0;
+    
+    for (int index = 1; index < myData.size(); index ++)
+    {
+        if (myData[minIndex] > myData[index])
+        {
+            minIndex = index;
+        }
+        
+        else if (myData[maxIndex] < myData[index])
+        {
+            maxIndex = index;
+        }
+    }
+    searchTimer.stopTimer();
+    cout << "The smallest Crime stat is at " << minIndex << " and it is: " << myData[minIndex] << endl;
+    cout << "The largest Crime stat is at " << maxIndex << " and it is: " << myData[maxIndex] << endl;
+    searchTimer.displayInformation();
 }
