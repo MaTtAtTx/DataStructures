b0de41c7f3f52f1505f4b02ba666c87350e48ab5
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-02-06 09:52:13 -0700

    Finished advanced array testing methods and created the node hpp file.

---
+++ DataStructures/Model/Nodes/Node.hpp
@@ -0,0 +1,54 @@
+//
+//  Node.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 2/6/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+#include <stdio.h>
+
+using namespace std;
+
+template <class Type>
+class Node
+{
+    //This section is available to ALL the subclasses of Node
+protected:
+    Type data;
+public:
+    Node();
+    Node(Type data);;
+    void setData(Type data);
+    Type getData();
+};
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Needed for default constructor complilation
+    //Since when we create a structure it does not have an item to store yet.
+}
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+void Node<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+#endif /* Node_hpp */
--- DataStructures/Testers/ArrayTester.cpp
+++ DataStructures/Testers/ArrayTester.cpp
@@ -21,9 +21,31 @@ void ArrayTester :: testArrayUse()
     {
         cout << firstArray[index] << endl;
     }
+    
+    Array<int> secondArray(400);
+    
+    cout << secondArray.getSize() << endl;
+    cout << firstArray.getSize() << endl;
+    
+    Array<int> thirdArray(secondArray);
+    
+    cout << thirdArray.getSize() << endl;
+    firstArray = thirdArray;
+    cout << firstArray[4] << endl;
+    
+    thirdArray[5] = 123;
+    cout << thirdArray[5] << endl;
 }
 
 void ArrayTester :: testAdvancedArray()
 {
+    Array<int> testArray(12);
+    for (int index = 0; index < testArray.getSize(); index++)
+    {
+        testArray.setAtIndex(index, index);
+    }
     
+    int testHolder;
+    testHolder = testArray.getFromIndex(7);
+    cout << "The value in testHolder is " << testHolder << endl;
 }
