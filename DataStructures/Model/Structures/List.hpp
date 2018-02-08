d821da13979ac74bde7ec6c6d04c9303cf16f113
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-02-08 09:38:01 -0700

    Created the list header file.

---
+++ DataStructures/Model/Structures/List.hpp
@@ -0,0 +1,35 @@
+//
+//  List.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 2/8/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+#include <iostream>
+#include <assert.h>
+#include "../Nodes/LinearNode.hpp"
+
+using namespace std;
+
+template <class Type>
+class List 
+{
+protected:
+    int size;
+public:
+    //Strcuture
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(Type item) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+};
+
+#endif /* List_hpp */
