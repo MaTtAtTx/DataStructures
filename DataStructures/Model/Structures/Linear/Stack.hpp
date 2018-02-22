c176648110d939ed2f1f600643ef3fb0d98d628b
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-02-22 08:09:29 -0700

    Created the stack header file and started working on it.

---
+++ DataStructures/Model/Structures/Linear/Stack.hpp
@@ -0,0 +1,33 @@
+//
+//  Stack.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 2/22/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Stack_hpp
+#define Stack_hpp
+
+#include "LinkedList.hpp"
+
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+public:
+    Stack();
+    ~Stack();
+    
+    //Stack specific methods
+    void push(Type data);
+    Type pop();
+    Type peek();
+    
+    //Overridden LinkedList methods
+    void add(Type data);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    Type remove(int index);
+};
+
+#endif /* Stack_hpp */
