0259553c9df9579ff92ff105a4606192498e93ef
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-02-26 09:20:44 -0700

    Created circular list header file and started implementinig its methods.

---
+++ DataStructures/Model/Structures/Linear/CircularList.hpp
@@ -0,0 +1,44 @@
+//
+//  CircularList.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef CircularList_hpp
+#define CircularList_hpp
+
+#include "List.hpp"
+#inlcude "../../Nodes/DoubleNode.hpp"
+
+template <class Type>
+class CircularList : public List<Type>
+{
+private:
+    DoubleNode<Type> * front;
+    DoubleNode<Type> * end;
+    
+    DoubleNode<Type> * findNode(int index)
+public:
+    CircularList();
+    ~CircularList();
+    
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type remove(int index);
+    Type getFromIndex(int index);
+    int getSize() const;
+};
+
+template <class Type>
+CircularList<Type> :: CircularList()
+{
+    front->setPrevious(end);
+    front->setNext(end);
+    
+    end->setPrevious(front);
+    end->setNext(front);
+}
+
+#endif /* CircularList_hpp */
