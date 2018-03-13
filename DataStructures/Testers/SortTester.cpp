6714fa8103bd65db5fa7be8acf1cbc667c4a893b
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-03-13 09:40:13 -0600

    Created the sort tester cpp and hpp.

---
+++ DataStructures/Testers/SortTester.cpp
@@ -0,0 +1,9 @@
+//
+//  SortTester.cpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 3/13/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "SortTester.hpp"
---
+++ DataStructures/Testers/SortTester.hpp
@@ -0,0 +1,21 @@
+//
+//  SortTester.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 3/13/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef SortTester_hpp
+#define SortTester_hpp
+
+#include <iostream>
+
+using namespace std;
+
+class SortTester
+{
+    
+};
+
+#endif /* SortTester_hpp */
