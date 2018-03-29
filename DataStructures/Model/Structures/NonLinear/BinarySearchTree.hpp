fbc4401a739cdbac19874a4cc8252b58321b3e6e
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-03-29 08:12:53 -0600

    Added the UML for Binary Search Tree and created the header file with all the method headers in the protected and public section.

---
+++ DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,60 @@
+//
+//  BinarySearchTree.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 3/29/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_hpp
+#define BinarySearchTree_hpp
+
+#include "Tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+template<class Type>
+class BinarySearchTree : public Tree<Type>
+{
+protected:
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * startNode, int index, int size);
+    
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removedNode(BinaryTreeNode<Type> * removeMe);
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    void setRoot(BinaryTreeNode<Type> * root);
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    void demoTraversalStep(BinaryTreeNode<Type> * node);
+    
+    int getSize();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
+    
+    void insert(Type itemToInsert);
+    bool contains(Type value);
+    void remove(Type value);
+    
+    Type findMinimum();
+    Type findMaximum();
+};
+
+#endif /* BinarySearchTree_hpp */
