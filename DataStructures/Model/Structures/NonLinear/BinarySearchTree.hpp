01657def6d2a2e9ccd7c6013d34a9aec94a28829
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-09 09:45:41 -0600

    Completed the contains and remove methods in the binary search tree class.

--- DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
+++ DataStructures/Model/Structures/NonLinear/BinarySearchTree.hpp
@@ -276,13 +276,85 @@ void BinarySearchTree<Type> :: insert(Type itemToInsert)
 template <class Type>
 bool BinarySearchTree<Type> :: contains(Type value)
 {
-    return false;
+    BinaryTreeNode<Type> * current = this->root;
+    if (current == nullptr)
+    {
+        return false;
+    }
+    else
+    {
+        while (current != nullptr)
+        {
+            if (value == current->getData())
+            {
+                return true;
+            }
+            else if (value < current->getData())
+            {
+                current = current->getLeftNode();
+            }
+            else
+            {
+                current = current->getRightNode();
+            }
+        }
+        return false;
+    }
 }
 
 template <class Type>
-void BinarySearchTree<Type> :: remove(Type item)
+void BinarySearchTree<Type> :: remove(Type value)
 {
-    
+    if (this->root == nullptr)
+    {
+        cout << "Empty tree so removal is not possible" << endl;
+    }
+    else
+    {
+        BinaryTreeNode<Type> * current = this->root;
+        BinaryTreeNode<Type> * previous = nullptr;
+        bool hasBeenFound = false;
+        
+        while (current != nullptr && !hasBeenFound)
+        {
+            if (current->getData() == value)
+            {
+                hasBeenFound = true;
+            }
+            else
+            {
+                previous = current;
+                if (value < current->getData())
+                {
+                    current = current->getLeftNode();
+                }
+                else
+                {
+                    current = current->getRightNode();
+                }
+            }
+        }
+        
+        if (current == nullptr)
+        {
+            cerr << "Item not found, removal unsuccessful" << endl;
+        }
+        else if (hasBeenFound)
+        {
+            if (current == this->root)
+            {
+                removeNode(this->root);
+            }
+            else if (value < previous->getData())
+            {
+                removeNode(previous->getLeftNode());
+            }
+            else
+            {
+                removeNode(previous->getRightNode());
+            }
+        }
+    }
 }
 
 #endif /* BinarySearchTree_hpp */
