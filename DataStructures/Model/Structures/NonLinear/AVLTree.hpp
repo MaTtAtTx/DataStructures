20b390fcc3d850fceed07bc8fb2cee810b5a7473
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-17 08:26:48 -0600

    Created the AVL Tree header file and started implementing the code.

---
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -0,0 +1,41 @@
+//
+//  AVLTree.hpp
+//  DataStructures
+//
+//  Created by Johnsen, Matthew on 4/17/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef AVLTree_hpp
+#define AVLTree_hpp
+
+#include "BinarySearchTree.hpp"
+
+template <class Type>
+class AVLTree : public BinarySearchTree<Type>
+{
+private:
+    BinaryTreeNode<Type> * leftRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * leftRightRotation(BinaryTreeNode<Type> * parent);
+    BinaryTreeNode<Type> * rightLeftRotation(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * balanceSubTree(BinaryTreeNode<Type> * parent);
+    
+    BinaryTreeNode<Type> * insertNode(BinaryTreeNode<Type> * parent, Type value);
+    BinaryTreeNode<Type> * removeNode(BinaryTreeNode<Type> * parent, Type value);
+    
+    int heightDifference(BinaryTreeNode<Type> * parent);
+
+public:
+    AVLTree();
+    
+    void insert(Type itemToInsert);
+    void remove(Type value);
+};
+
+/*
+ A negative value means the right is greater than the left
+ */
+
+#endif /* AVLTree_hpp */
