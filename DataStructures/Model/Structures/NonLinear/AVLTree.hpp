ca1592778464808aa12cbea3a162ffd577c97737
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-17 09:03:33 -0600

    Finished another method in the AVLTree header file.

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -99,6 +99,31 @@ template <class Type>
 BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree(BinaryTreeNode<Type> * parent)
 {
     int balanceFactor = heightDifference(parent);
+    
+    if (balanceFactor > 1)
+    {
+        if (heightDifference(parent->getLeftNode()) > 0)
+        {
+            parent = leftRotation(parent);
+        }
+        else
+        {
+            parent = leftRightRotation(parent);
+        }
+    }
+    else if (balanceFactor < -1)
+    {
+        if (heightDifference(parent->getRightNode()) > 0)
+        {
+            parent = rightLeftRotation(parent);
+        }
+        else
+        {
+            parent = rightRotation(parent);
+        }
+    }
+    
+    return parent;
 }
 
 #endif /* AVLTree_hpp */
