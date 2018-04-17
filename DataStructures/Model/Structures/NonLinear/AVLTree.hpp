d8bbf1e4d792333d6e5d72f6459ac3b4e8a2de83
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-17 09:15:31 -0600

    Finished the removeNode method in the AVLTree header file.

--- DataStructures/Model/Structures/NonLinear/AVLTree.hpp
+++ DataStructures/Model/Structures/NonLinear/AVLTree.hpp
@@ -126,4 +126,51 @@ BinaryTreeNode<Type> * AVLTree<Type> :: balanceSubTree(BinaryTreeNode<Type> * pa
     return parent;
 }
 
+template <class Type>
+BinaryTreeNode<Type> * AVLTree<Type> :: removeNode(BinaryTreeNode<Type> * parent, Type inserted)
+{
+    if (parent == nullptr)
+    {
+        return parent;
+    }
+    if (inserted < parent->getNodeData())
+    {
+        parent->setLeftChild(removeNode(parent->getLeftNode(), inserted));
+    }
+    else if (inserted > parent->getNodeData())
+    {
+        parent->setRightChild(removeNode(parent->getRightNode(), inserted));
+    }
+    else
+    {
+        BinaryTreeNode<Type> * temp;
+        if (parent->getLeftNode() == nullptr && parent->getRightNode() == nullptr)
+        {
+            temp = parent;
+            delete temp;
+        }
+        else if (parent->getLeftNode() == nullptr)
+        {
+            *parent = *parent->getRightNode();
+        }
+        else if (parent->getRightNode() == nullptr)
+        {
+            *parent = *parent->getLeftNode();
+        }
+        else
+        {
+            BinaryTreeNode<Type> * leftMost = this->getLeftMostChild(parent->getRightNode);
+            parent->setNodeData(leftMost->getNodeData());
+            parent->setRightChild(removeNode(parent->getRightNode(), leftMost->getNodeData()));
+        }
+    }
+    
+    if (parent == nullptr)
+    {
+        return parent;
+    }
+    
+    return balanceSubTree(parent);
+}
+
 #endif /* AVLTree_hpp */
