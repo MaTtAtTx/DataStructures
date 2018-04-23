f395018a977eb3850e37e00c7305e8904223f05e
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-04-23 08:45:46 -0600

    Wrote some of the methods for the N_AryTreeNode header file.

--- DataStructures/Model/Nodes/N_AryTreeNode.hpp
+++ DataStructures/Model/Nodes/N_AryTreeNode.hpp
@@ -18,25 +18,67 @@ template <class Type>
 class N_AryTreeNode : public Node<Type>
 {
 private:
-    set<N_AryTreeNode<Type>*> nodes;
+    set<N_AryTreeNode<Type> *> nodes;
     N_AryTreeNode<Type> * root;
 public:
     N_AryTreeNode<Type>();
     ~N_AryTreeNode<Type>();
     N_AryTreeNode<Type>(Type data);
     
-    set<N_AryTreeNode<Type>*> getNodes();
+    set<N_AryTreeNode<Type> *> getNodes();
     N_AryTreeNode<Type> * getRoot();
     int getChildCount();
     
     void setRoot(N_AryTreeNode<Type> * root);
-    void addChild(N_AryTreeNode<Type> * node);
+    void addChild(Type data);
 };
 
 template <class Type>
 N_AryTreeNode<Type> :: N_AryTreeNode()
 {
     //Unused
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: N_AryTreeNode(Type data) : Node<Type>(data)
+{
+}
+
+template <class Type>
+N_AryTreeNode<Type> :: ~N_AryTreeNode()
+{
+    nodes.clear();
+}
+
+template <class Type>
+N_AryTreeNode<Type> * N_AryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+set<N_AryTreeNode<Type> *> N_AryTreeNode<Type> :: getNodes()
+{
+    return nodes;
+}
+
+template <class Type>
+int N_AryTreeNode<Type> :: getChildCount()
+{
+    return nodes.size();
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: setRoot(N_AryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void N_AryTreeNode<Type> :: addChild(Type Data)
+{
+    N_AryTreeNode<Type> * node = new N_AryTreeNode<Type>(data);
+    nodes.insert(node);
 }
 
 #endif /* N_AryTreeNode_hpp */
