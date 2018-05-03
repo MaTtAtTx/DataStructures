51507d38d4921b45c508b6991fde37d5f1ea30be
mattitude17 <31513806+mattitude17@users.noreply.github.com>
2018-05-03 07:50:51 -0600

    Added code for resize method to hashtable.

--- DataStructures/Model/Structures/NonLinear/Hashtable.hpp
+++ DataStructures/Model/Structures/NonLinear/Hashtable.hpp
@@ -114,7 +114,36 @@ long Hashtable<Type> :: getSize()
 template <class Type>
 void Hashtable<Type> :: resize()
 {
+    long updatedCapacity = nextPrime();
+    HashNode<Type> * * tempStorage = new HashNode<Type> * [updatedCapacity];
     
+    std :: fill_n(tempStorage, updatedCapacity, nullptr);
+    
+    long oldCapacity = this->capacity;
+    this->capacity = updatedCapacity;
+    
+    for (ong index = 0; index < oldCapacity; index++)
+    {
+        if (hashTalbeStorage[index] != nullptr)
+        {
+            HashNode<Type> * temp = internalStorage[index];
+            
+            long position = findPosition(temp);
+            if (tempStorage[position] == nullptr)
+            {
+                tempStorage[position] = temp;
+            }
+            else
+            {
+                long updatedPosition = handleCollision(temp, position);
+                if (updatedPosition != -1)
+                {
+                    tempStorage[updatedPosition] = temp;
+                }
+            }
+        }
+    }
+    internalStorage = tempStorage;
 }
 
 template <class Type>
